10bd4ea99badb664ad3ecba58435106c9085c912
nella <147390743+prazakovaa@users.noreply.github.com>
2024-06-03 10:58:52 +0200

    final2

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-radio.setFrequencyBand(73)
+radio.setFrequencyBand(72)
 radio.setTransmitPower(5)
 radio.setGroup(73)
 radio.setTransmitSerialNumber(true)
@@ -12,11 +12,14 @@ enum Pins {
     trig = DigitalPin.P2,
     echo = DigitalPin.P1
 }
+
 enum ServoDirection {
     Left = 2,
     Center = 1,
     Right = 0
 }
+
+/*
 const allIRPins: Array<number> = [Pins.wr, Pins.wl, Pins.r, Pins.l, Pins.c, Pins.trig]
 for (let pin of allIRPins) {
     pins.setPull(pin, PinPullMode.PullNone);
@@ -35,51 +38,70 @@ const servoMove = (direction: ServoDirection): void => {
     PCAmotor.StopServo(PCAmotor.Servos.S1)
 }
 
-///
+*/
 
+basic.showLeds(`
+. # # . .
+. # # # .
+. # # # #
+. # . . .
+. # . . .
+`)
+
+let stripHead = neopixel.create(DigitalPin.P0, 9, NeoPixelMode.RGB)
+let strip = neopixel.create(DigitalPin.P16, 9, NeoPixelMode.RGB)
+strip.showColor(neopixel.rgb(255, 0, 0))
+stripHead.showColor(neopixel.rgb(255, 0, 0))
+strip.show()
+
+// basic.forever(function () {
+//     PCAmotor.GeekServo(PCAmotor.Servos.S1, 800)
+//     basic.pause(8000)
+//     PCAmotor.GeekServo(PCAmotor.Servos.S1, 2200)
+//     basic.pause(8000)
+// })
 
 type Protokol = {
     x: number; //smer
     y: number; //rychlost
-    // a: boolean;
-    // b: boolean;
-    // logo: boolean
 }
 
-let letter: string = ""
+let letter: string
 
-radio.onReceivedString( function (received: string) {
+radio.onReceivedString(function (receivedString: string) {
+    letter = receivedString
 
-    letter = received
-
-    let article = letter.split(";")
     let data: Protokol = {
-        x: parseInt(article[0]),
-        y: parseInt(article[1]),
-        // a: article[2] === "1",
-        // b: article[3] === "1",
-        // logo: article[4] === "1"
+        x: parseInt(letter.split(";")[0]),
+        y: parseInt(letter.split(";")[1])
     }
 
-    let direction = Math.map(data.x, -1024, 1023, -120, 120);
-    let speed = Math.map(data.y, -1024, 1023, -255, 255);
+    let turns = Math.map(data.x, -1024, 1024, -120, 120)
+    let run = Math.map(data.y, -1024, 1024, -255, 255)
 
-    if (data.x <= 0){
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -speed)
-    } else if (data.x >= 0) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
+    const runMotors = (motor1: PCAmotor.Motors, motor1Speed: number, motor2: PCAmotor.Motors, motor2Speed: number) => {
+        PCAmotor.MotorRun(motor1, motor1Speed)
+        PCAmotor.MotorRun(motor2, motor2Speed)
     }
 
-    if (data.x <= 0 && data.y >= 0){
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed -direction)
-    } else if (data.x >= 0 && data.y >= 0) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -speed -direction)
-    } else if (data.x >= 0 && data.y <= 0) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -speed + direction)
-    } else if (data.x <= 0 && data.y <= 0) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed + direction)
+    let motor1Speed = 0
+    let motor2Speed = 0
+
+    if (data.x <= 0) {
+        motor1Speed = -run
+        if (data.y >= 0) {
+            motor2Speed = run - turns
+        } else {
+            motor2Speed = run + turns
+        }
+        runMotors(PCAmotor.Motors.M1, motor1Speed, PCAmotor.Motors.M4, motor2Speed)
+    } else {
+        motor2Speed = run
+        if (data.y >= 0) {
+            motor1Speed = -run - turns
+        } else {
+            motor1Speed = -run + turns
+        }
+        runMotors(PCAmotor.Motors.M4, motor2Speed, PCAmotor.Motors.M1, motor1Speed)
     }
-    
-
-
 })
